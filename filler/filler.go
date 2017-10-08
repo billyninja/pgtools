50fcf8de2517b426f9f01e26515d977c4fbd091c
billyninja <joaoeduardocm@gmail.com>
2017-10-08 17:15:34 -0300

    almost there with basic random insert values. Todo next: limit var char correctly

--- filler/filler.go
+++ filler/filler.go
@@ -10,34 +10,58 @@ import (
 	"time"
 )
 
+func rndColumn(cl *scanner.Column) string {
+
+    switch cl.Type {
+        case "numeric":
+            v := rnd.PSQL_numeric(99999.99, 2)
+            return fmt.Sprintf("%.2f", v)
+        case "character varying":
+            return rnd.PSQL_var_char(3, 3)
+        case "date":
+            return rnd.PSQL_datetime(1, 2)
+        case "timestamp without time zone":
+            return rnd.PSQL_datetime(0, 0)
+        default:
+            fmt.Printf("\n\nRAND DATA-TYPE NOT IMPLEMENTED: %s\n\n", cl.Type)
+            break
+    }
+
+    return ""
+}
+
 func BaseInsertQuery(tb *scanner.Table, skip_nullable uint8) string {
 	base := fmt.Sprintf(`INSERT INTO "%s" (`, tb.Name)
-
-    for _, c := range tb.Columns {
-        if skip_nullable > 0 && c.Nullable == "YES" {
+    values := "VALUES ("
+    for _, cl := range tb.Columns {
+        if skip_nullable > 0 && cl.Nullable == "YES" {
             continue
         }
-		base += c.Name
+		base += cl.Name
 		base += ", "
+        values += rndColumn(cl)
+        values += ", "
 	}
     base = base[0:len(base) - 2]
+    values = values[0:len(values) - 2]
 
-    v1 := fmt.Sprintf(
-        "(%s, %s, %s, %s, %s, %s, %s, %.2f, %.2f, %.2f , '{}', FALSE)",
-        rnd.PSQL_var_char(3, 3),
-        rnd.PSQL_var_char(3, 3),
-        rnd.PSQL_datetime(1, 2),
-        rnd.PSQL_datetime(1, 2),
-        rnd.PSQL_datetime(0, 0),
-        rnd.PSQL_datetime(0, 0),
-        rnd.PSQL_var_char(2, 2),
-        rnd.PSQL_numeric(99999.99, 2),
-        rnd.PSQL_numeric(99.99, 2),
-        rnd.PSQL_numeric(99.99, 2),
-    )
+    // v1 := fmt.Sprintf(
+    //     "(%s, %s, %s, %s, %s, %s, %s, %.2f, %.2f, %.2f , '{}', FALSE)",
+    //     rnd.PSQL_var_char(3, 3),
+    //     rnd.PSQL_var_char(3, 3),
+    //     rnd.PSQL_datetime(1, 2),
+    //     rnd.PSQL_datetime(1, 2),
+    //     rnd.PSQL_datetime(0, 0),
+    //     rnd.PSQL_datetime(0, 0),
+    //     rnd.PSQL_var_char(2, 2),
+    //     rnd.PSQL_numeric(99999.99, 2),
+    //     rnd.PSQL_numeric(99.99, 2),
+    //     rnd.PSQL_numeric(99.99, 2),
+    // )
 
-	base += ") VALUES "
-    base += v1
+	base += ") "
+    base += values
+    base += ")"
 
 	return base
 }
